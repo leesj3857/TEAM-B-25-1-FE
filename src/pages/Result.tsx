1e11476a51d571e60360529febeb88db63cbdede
이승준 <hensin12@gmail.com>
2025-09-08 17:22:24 +0900

    fix: result페이지에서 새로고침 시 전역상태에 inviteCode 저장 추가

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,6 +1,17 @@
 import Result from '../feature/result';
+import { useParams } from 'react-router-dom';
+import { useInviteCode } from '../context/inviteCodeContext';
+import { useEffect } from 'react';
 
 const ResultPage = () => {
+  const { inviteCode: urlInviteCode } = useParams();
+  const { setInviteCode, inviteCode } = useInviteCode();
+  useEffect(() => {
+    if (urlInviteCode && !inviteCode) {
+      setInviteCode(urlInviteCode);
+    }
+  }, [urlInviteCode, setInviteCode, inviteCode]);
+  
   return <Result />;
 };
 
